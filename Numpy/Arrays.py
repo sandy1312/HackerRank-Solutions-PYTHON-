0e23486419cd49cbf01b89b644b37a3f68ed14c7
Kumar Sunderam <68020132+sandy1312@users.noreply.github.com>
2020-11-09 22:19:47 +0530

    Add files via upload

---
+++ Numpy/Arrays.py
@@ -0,0 +1,7 @@
+import numpy
+b=list(map(int,input().split()))
+print(numpy.reshape(b,(3,3)))
+
+
+
+
---
+++ Numpy/Shape and Reshape.py
@@ -0,0 +1,6 @@
+import numpy
+b=list(map(int,input().split()))
+print(numpy.reshape(b,(3,3)))
+
+
+
